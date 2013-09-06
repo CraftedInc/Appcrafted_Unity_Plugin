6b86c2290d60baa8835989a088d805cdb4987c39
Brandon Wu <plinan@BLM.local>
2013-09-05 17:26:28 -0700

    message 2

--- AdSpace.cs
+++ AdSpace.cs
@@ -13,36 +13,5 @@ public class AdSpace : MonoBehaviour //MonoBehaviour so that we can use Coroutin
 		adSpaceID = ID;
 	}
 	
-	/*
-	IEnumerator LoadJSON (string JSONurl) {
-		
-		Debug.Log ("LoadJSON called");
-				
-		WWW www = new WWW(JSONurl);	// Start a download of the given URL
-		yield return www;			// wait until the download is done
-		JSONObject adSpaceJSONObject = JSONObject.Parse(www.text); //define the JSON Object
-		
-		int numberOfAds = adSpaceJSONObject.GetArray("Ads").Length;
-		
-		//load ads into an array
-		for (int i = 0; i < numberOfAds; i++)
-		{
-			JSONObject thisAd = JSONObject.Parse(adSpaceJSONObject.GetArray("Ads")[i].ToString());
-			
-			//get the image from the image link
-			WWW wwwImage = new WWW(thisAd.GetString ("image")); // "image" contains a link to the image
-			yield return wwwImage;
-			craftedAds[i].image = wwwImage.texture;
-			
-			//get all other values
-			craftedAds[i].title = thisAd.GetString ("title");
-			craftedAds[i].text = thisAd.GetString ("text");
-			craftedAds[i].link = thisAd.GetString ("link");
-		}
-		
-		Debug.Log (	"craftedAds array in AdSpace: " +
-					"\n" + craftedAds);
-	}
-	*/
 	
 }
