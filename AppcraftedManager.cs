f9694395b456cf23864427c48bdb3a73d0fb0baf
Brandon Wu <brandon.wu.com@gmail.com>
2014-01-13 20:22:57 -0800

    Adding support for iOS
    integrating unityweb, modified Response.cs a bit and removed DLL folder
    to avoid random compilation errors when building for iOS.

--- AppcraftedManager.cs
+++ AppcraftedManager.cs
@@ -34,12 +34,10 @@ using System.Text;
 namespace CraftedInc.Appcrafted
 {
 	class Container {
-		public Dictionary<string, Asset> assets 
-			= new Dictionary<string, Asset>();
+		public Dictionary<string, Asset> assets = new Dictionary<string, Asset>();
 	}
 	class Asset {
-		public Dictionary<string, object> attributes 
-			= new Dictionary<string, object>();
+		public Dictionary<string, object> attributes = new Dictionary<string, object>();
 	}
 	class AppcraftedManager : MonoBehaviour { //MonoBehaviour required for coroutine
 		private string endpoint = "http://api.appcrafted.com/v0/assets/";
@@ -49,7 +47,7 @@ namespace CraftedInc.Appcrafted
 		private bool isResetting = false;
 		public delegate	void AssetDelegate(Asset asset);
 		public static event AssetDelegate OnLoaded;
-
+		
 		//create a singleton that doesn't need to be attached to a gameobject
 		#region Create Singleton
 		private static AppcraftedManager instance = null;
@@ -78,13 +76,13 @@ namespace CraftedInc.Appcrafted
 			}
 		}
 		#endregion
-
+		
 		//a public method to register credentials
 		public void RegisterCredentials(string accessKey, string secretKey) {
 			this.accessKey = accessKey;
 			this.secretKey = secretKey;
 		}
-
+		
 		//retrieves the specified Asset.
 		public void GetAsset(string containerID, string assetID){
 			try {
@@ -99,7 +97,7 @@ namespace CraftedInc.Appcrafted
 				}
 			}
 		}
-
+		
 		//reset container.
 		public void ClearAssetCache(string containerID, string assetID){
 			if (!isResetting){
@@ -107,121 +105,154 @@ namespace CraftedInc.Appcrafted
 				StartCoroutine(RetrieveAsset(containerID, assetID));
 			}
 		}
-
+		
 		//retrive all assets in a container 
 		private IEnumerator RetrieveAsset(string containerID, string assetID) {
-
+			
 			isResetting = true;
 
-			//add container
-			Container container = new Container();
-			this.containers.Add(containerID, container);
+			JSONObject containerJSON = new JSONObject();
+			string url = this.endpoint + containerID + "/all";
 
+			//add container
+			if (!this.containers.ContainsKey(containerID)){
+				Container container = new Container();
+				this.containers.Add(containerID, container);
+			}
+			
 			//validate credentials
 			if (this.accessKey == null || this.secretKey == null) {
 				throw new System.MemberAccessException("missing credentials");
 			}
+#if UNITY_IPHONE
+
+			var request = new HTTP.Request("GET", url);
+			request.SetHeader("Authorization", "Basic " + System.Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes(this.accessKey + ":" + this.secretKey)));
+			request.Send();
+			while(!request.isDone)
+			{
+				yield return new WaitForEndOfFrame();
+			}
+			if (request.exception!=null){
+				Debug.Log(request.exception);
+			} else {
+				var response = request.response;
+				//inspect response code
+				Debug.Log(response.status);
+				//inspect headers
+				Debug.Log(response.GetHeader("Content-Type"));
+				//Get the body as a byte array
+				//Debug.Log(response.bytes);
+				//Or as a string
+				Debug.Log(response.Text);
+
+				containerJSON = JSONObject.Parse(response.Text); 
+			}
+
+
+#else
+
 			Hashtable headers = new Hashtable();
 			headers["Authorization"] = "Basic " +
 				System.Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes(
 					this.accessKey + ":" + this.secretKey));
-			string url = this.endpoint + containerID + "/all";			
 			WWW www = new WWW(url, null, headers);
 			yield return www;
+			containerJSON = JSONObject.Parse(www.text);
 
-			JSONObject containerJSON = JSONObject.Parse(www.text); 
-
+#endif			 
+			
 			//adding assets
 			for (int i = 0; i < containerJSON.GetArray("Assets").Length; i++){
-
+				
 				JSONObject assetJSON = JSONObject.Parse(containerJSON.GetArray("Assets")[i].ToString());
-
+				
 				Asset asset = new Asset();
 				string currentAssetID = assetJSON.GetString("AssetID");
-				this.containers[containerID].assets.Add(currentAssetID, asset);
+				if (!this.containers[containerID].assets.ContainsKey(currentAssetID)){
+					this.containers[containerID].assets.Add(currentAssetID, asset);
+				}
 				Debug.Log ("--- New asset: " + currentAssetID + " ---");
-
+				
 				//adding attributes
 				foreach (var keyValuePair in assetJSON.values) {
 					if (keyValuePair.Value.Type == JSONValueType.Object){ //this is how we know if the pair is an attribute pair and not meta data
 						string attributeName = keyValuePair.Key;
-						JSONObject attributeJSON = JSONObject.Parse(keyValuePair.Value.ToString());
-						//process attributes based on Type
-						string type = attributeJSON.GetString("Type");
-						switch (type)
-						{
-						case "URL":
-						case "STRING":
-							string value = attributeJSON.GetString ("Value");
-							this.containers[containerID].assets[currentAssetID]
-							.attributes.Add(attributeName, value);
-							Debug.Log (attributeName + " : " 
-							           + this.containers[containerID]
-							           .assets[currentAssetID]
-							           .attributes[attributeName]);
-							break;
-						case "IMAGE":
-							string imageURL = attributeJSON.GetString ("Value");
-							WWW imageObject = new WWW(imageURL); 
-							yield return imageObject;
-							Texture2D image = new Texture2D(imageObject.texture.width, imageObject.texture.height);
-							imageObject.LoadImageIntoTexture(image);
-							this.containers[containerID].assets[currentAssetID]
-							.attributes.Add(attributeName, image);
-							Debug.Log (attributeName + " : " 
-							           + this.containers[containerID]
-							           .assets[currentAssetID]
-							           .attributes[attributeName]);
-							break;
-						case "NUMBER":
-							double number = attributeJSON.GetNumber("Value");
-							this.containers[containerID].assets[currentAssetID]
-							.attributes.Add(attributeName, number);
-							Debug.Log (attributeName + " : " 
-							           + this.containers[containerID]
-							           .assets[currentAssetID]
-							           .attributes[attributeName]);
-							break;
-						case "FILE":
-							object file = attributeJSON.GetObject("Value");
-							this.containers[containerID].assets[currentAssetID]
-							.attributes.Add(attributeName, file);
-							Debug.Log (attributeName + " : " 
-							           + this.containers[containerID]
-							           .assets[currentAssetID]
-							           .attributes[attributeName]);
-							break;
-						case "NUMBER_ARRAY":
-							int numberArrayLength = attributeJSON.GetArray("Value").Length;
-							double[] number_array = new double[numberArrayLength];
-							for (int j = 0; j<numberArrayLength ;j++){
-								number_array[j] = attributeJSON.GetArray("Value")[j].Number ;
+
+
+						if (!this.containers[containerID].assets[currentAssetID]
+						    .attributes.ContainsKey(attributeName)){
+
+							JSONObject attributeJSON = JSONObject.Parse(keyValuePair.Value.ToString());
+							//process attributes based on Type
+							string type = attributeJSON.GetString("Type");
+							switch (type)
+							{
+							case "URL":
+							case "FILE": 
+							case "STRING":
+								string value = attributeJSON.GetString ("Value");
+								this.containers[containerID].assets[currentAssetID]
+								.attributes.Add(attributeName, value);
+								Debug.Log (attributeName + " : " 
+								           + this.containers[containerID]
+								           .assets[currentAssetID]
+								           .attributes[attributeName]);
+								break;
+							case "IMAGE":
+								string imageURL = attributeJSON.GetString ("Value");
+								WWW imageObject = new WWW(imageURL); 
+								yield return imageObject;
+								Texture2D image = new Texture2D(imageObject.texture.width, imageObject.texture.height);
+								imageObject.LoadImageIntoTexture(image);
+								this.containers[containerID].assets[currentAssetID]
+								.attributes.Add(attributeName, image);
+								Debug.Log (attributeName + " : " 
+								           + this.containers[containerID]
+								           .assets[currentAssetID]
+								           .attributes[attributeName]);
+								break;
+							case "NUMBER":
+								double number = attributeJSON.GetNumber("Value");
+								this.containers[containerID].assets[currentAssetID]
+								.attributes.Add(attributeName, number);
+								Debug.Log (attributeName + " : " 
+								           + this.containers[containerID]
+								           .assets[currentAssetID]
+								           .attributes[attributeName]);
+								break;
+							case "NUMBER_ARRAY":
+								int numberArrayLength = attributeJSON.GetArray("Value").Length;
+								double[] number_array = new double[numberArrayLength];
+								for (int j = 0; j<numberArrayLength ;j++){
+									number_array[j] = attributeJSON.GetArray("Value")[j].Number ;
+								}
+								this.containers[containerID].assets[currentAssetID]
+								.attributes.Add(attributeName, number_array);
+								break;
+							case "STRING_ARRAY":
+								int stringArrayLength = attributeJSON.GetArray("Value").Length;
+								string[] string_array = new string[stringArrayLength];
+								for (int k = 0; k<stringArrayLength ;k++){
+									string_array[k] = attributeJSON.GetArray("Value")[k].Str ;
+								}
+								this.containers[containerID].assets[currentAssetID]
+								.attributes.Add(attributeName, string_array);
+								break;
 							}
-							this.containers[containerID].assets[currentAssetID]
-							.attributes.Add(attributeName, number_array);
-							break;
-						case "STRING_ARRAY":
-							int stringArrayLength = attributeJSON.GetArray("Value").Length;
-							string[] string_array = new string[stringArrayLength];
-							for (int k = 0; k<stringArrayLength ;k++){
-								string_array[k] = attributeJSON.GetArray("Value")[k].Str ;
-							}
-							this.containers[containerID].assets[currentAssetID]
-							.attributes.Add(attributeName, string_array);
-							break;
 						}
 					}
 				}
 			}
-
+			
 			//trigger event OnLoaded
 			if (OnLoaded != null) {
 				OnLoaded(this.containers[containerID].assets[assetID]); 
 			}
-
+			
 			isResetting = false;
 		}
-
+		
 	}
-
+	
 }
---
+++ unityweb/README.md
@@ -0,0 +1,8 @@
+# [unityweb](https://code.google.com/p/unityweb/)
+A better way to handle HTTP in Unity3D.
+
+**Source**: [https://code.google.com/p/unityweb/](https://code.google.com/p/unityweb/)
+**Code license**: [GNU GPL v2](http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+**By**: simonwit...@gmail.com [https://code.google.com/u/112476354298040130065/](https://code.google.com/u/112476354298040130065/)
+
+_Used under the [GNU GPL v2 license](http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)._
